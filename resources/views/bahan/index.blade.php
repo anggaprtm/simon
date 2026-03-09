664ccd49694ac0f3e14a8b9b870fe9e87f87066d
Angga Pratama <angga.iryanto@staf.unair.ac.id>
2026-03-09 08:09:08 +0700

    🐛fix: fix tumpang tindih icon dan tulisan pencarian

--- resources/views/bahan/index.blade.php
+++ resources/views/bahan/index.blade.php
@@ -127,7 +127,7 @@
                             <label class="block text-xs font-bold text-gray-500 uppercase tracking-wider mb-1.5">Cari Bahan</label>
                             <div class="relative">
                                 <svg class="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/></svg>
-                                <input type="text" id="search-input" name="search" value="{{ $search ?? '' }}" placeholder="Ketik Kode, nama, merk, jenis..." class="search-input pl-9" autocomplete="off">
+                                <input type="text" id="search-input" name="search" value="{{ $search ?? '' }}" placeholder="Ketik Kode, nama, merk, jenis..." class="search-input !pl-10" autocomplete="off">
                             </div>
                         </div>
                         <div class="md:col-span-3 flex gap-2">
